0ef1f40dc0368a469c668a6c706b9c8707af732a
Lucas <lucasazzollinivieira@gmail.com>
2022-05-08 23:53:49 -0300

    chore: eslint configs added

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:cypress/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "cypress"
+    ],
+    "rules": {
+        "jest/expect-expect": "off"
+    }
+}
