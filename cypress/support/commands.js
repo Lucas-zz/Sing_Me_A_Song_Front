db53c420a4e8c0e8722cd60a92abed43fb993001
iagod <iago.tostes@cubos.io>
2022-06-29 10:26:02 -0300

    Preparando revisão de código

--- cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add("clearDatabase", () => {
-    cy.request("POST", Cypress.config().apiUrl + `/reset-database`).as("dropDatabase");
-});
-
-Cypress.Commands.add("seedRecommendations", () => {
-    cy.request("POST", Cypress.config().apiUrl + `/seed/recommendations`).as("seedDatabase");
-});
